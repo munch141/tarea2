cc541b091abd98dce02ad5da6b84560e477b9c0d
ricardo <ricardo@ricardo-pc>
2016-04-27 04:21:58 -0430

    Primer commit

---
+++ tarea2/calcularPrecio.py
@@ -0,0 +1,34 @@
+'''
+Created on Apr 26, 2016
+
+@author: ricardo
+'''
+
+import math
+import datetime
+from decimal import Decimal
+
+class Tarifa:
+    def __init__(self, t_sem, t_fin):
+        self.sem = Decimal(t_sem)
+        self.fin = Decimal(t_fin)
+
+def calcularPrecio(tarifa, tiempoDeTrabajo):
+    ini = tiempoDeTrabajo[0]
+    fin = tiempoDeTrabajo[1]
+    
+    t = fin - ini
+    t = t.total_seconds()
+    
+    if t < 900:
+        horas = 0
+    elif 604800 < t:
+        horas = 168
+    else:
+        horas = int(math.ceil(t / 3600))
+    
+    if horas < 0:
+        print("Error: fecha de inicio posterior a fecha de fin.")
+        return -1
+    
+    return horas
---
+++ tarea2/testCalcularPrecio.py
@@ -0,0 +1,29 @@
+'''
+Created on Apr 26, 2016
+
+@author: ricardo
+'''
+import unittest
+import datetime
+from decimal import Decimal
+from calcularPrecio import Tarifa
+
+
+class TestCalcularPrecio(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
