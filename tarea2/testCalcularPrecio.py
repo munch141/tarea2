58a23bce579af926dc88b737c40d14641ee73e61
ricardo <ricardo@ricardo-pc>
2016-04-28 16:26:28 -0430

    Se qgregó prueba para caso en que el trabajo es mayor de 7 dias

--- tarea2/testCalcularPrecio.py
+++ tarea2/testCalcularPrecio.py
@@ -39,6 +39,12 @@ class TestCalcularPrecio(unittest.TestCase):
         ini = datetime.datetime(2016, 4, 1, 7, 0)
         fin = datetime.datetime(2016, 4, 1, 7, 5)
         self.assertEqual(0, calcularPrecio(tarifa, [ini, fin]))
+        
+    def testTimedeltaMayorA7Dias(self):
+        tarifa = Tarifa(1,1)
+        ini = datetime.datetime(2016, 4, 1, 7, 0)
+        fin = datetime.datetime(2016, 4, 15, 7, 0)
+        self.assertEqual(168, calcularPrecio(tarifa, [ini, fin]))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
