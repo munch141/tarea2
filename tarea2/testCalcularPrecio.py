369cc803aabb0d56b0ef95ba5546839237c529aa
giuli <giuli@giuli-VIT-P3400>
2016-04-28 14:43:23 -0430

    Prueba para el caso donde la tasa del día de semana es negativa

--- tarea2/testCalcularPrecio.py
+++ tarea2/testCalcularPrecio.py
@@ -5,15 +5,19 @@ Created on Apr 26, 2016
 '''
 import unittest
 import datetime
-from calcularPrecio import Tarifa
+from calcularPrecio import *
 
 
 class TestCalcularPrecio(unittest.TestCase):
     
-    def testName(self):
-        pass
-
+    def testTarifaSemNeg(self):
+        tarifa = Tarifa(-1,1)
+        ini = datetime.datetime(2016, 4, 1, 7, 0)
+        fin = datetime.datetime(2016, 4, 3, 9, 0)
+        self.assertEqual(-1, calcularPrecio(tarifa, [ini, fin]))
+    
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
+    
